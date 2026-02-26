3180a379efc6ad0d6e347440eca7f8a2b8475d6d
Sergio López Tapia <sergiolopeztapia@gmail.com>
2026-02-26 18:53:08 +0100

    Arreglar conexion con database y errores de linter

--- eslint.config.js
+++ eslint.config.js
@@ -3,37 +3,25 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-import { FlatCompat } from "@eslint/eslintrc";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDir: __dirname,
-});
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat["jsx-runtime"],
-  ...compat.extends("standard"),
   {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser }
+  },
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.{jsx,tsx}"],
+    ...pluginReact.configs.flat.recommended,
     settings: {
       react: {
         version: "detect",
       },
     },
     rules: {
-      "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }],
-    },
-  },
-  {
-    files: ["**/*.{jsx,tsx}"],
-    rules: {
-      "no-undef": "off",
+      "react/react-in-jsx-scope": "off",
     },
   },
 ]);
